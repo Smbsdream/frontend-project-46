9205cf1ab75c01ee075c941648767eb03dda3117
kirillov <buyback@inbox.ru>
2024-06-26 17:13:43 +0300

    add arg, option to gendiff cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
