315425603d7dd9f435d09c8c5ae6d3f53ca6c7d0
kirillov <buyback@inbox.ru>
2024-07-10 10:31:21 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,6 @@
 import { program } from 'commander';
 import genDiff from '../src/index.js';
 
-const options = program.opts();
-
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
@@ -13,9 +11,9 @@ program
 
 program
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
