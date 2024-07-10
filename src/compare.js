d0f6c07955b0594a4dc4b709cc7419b6c732180a
kirillov <buyback@inbox.ru>
2024-07-10 10:30:21 +0300

    add compare.js

---
+++ src/compare.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+
+const compareObjects = (data1, data2) => {
+  const unionKeys = Object.keys({ ...data1, ...data2 });
+  const sortKeys = _.sortBy(unionKeys);
+
+  const result = sortKeys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `\n - ${key}: ${data1[key]}`;
+    } else if (!_.has(data1, key)) {
+      return `\n + ${key}: ${data2[key]}`;
+    } else if (data1[key] !== data2[key]) {
+      return `\n - ${key}: ${data1[key]}\n + ${key}: ${data2[key]}`;
+    } else {
+      return `\n   ${key}: ${data1[key]}`;
+    }
+  });
+
+  return `{ ${result} \n}`;
+};
+
+export default compareObjects;
