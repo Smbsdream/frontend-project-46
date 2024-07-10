fae49e27e735186af58b44bcf201e32a53c4b0de
kirillov <buyback@inbox.ru>
2024-07-10 10:31:50 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import { getExtension, readFile } from './tools.js';
+import parse from './parsers.js';
+import compareObjects from './compare.js';
 
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+const genDiff = (filepath1, filepath2, formatName) => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
+
+  const object1 = parse(file1, getExtension(filepath1));
+  const object2 = parse(file2, getExtension(filepath2));
+
+  const compare = compareObjects(object1, object2);
+
+  return compare;
 };
+
+export default genDiff;
