e4f74f6243b884fd941cbd01d23ad4f435006e48
kirillov <buyback@inbox.ru>
2024-09-04 16:37:18 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,24 +7,57 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
-const filepath3 = getFixturePath('file1.yaml');
-const filepath4 = getFixturePath('file2.yml');
+const cases = [
+  {
+    filepath1: 'file1.json',
+    filepath2: 'file2.json',
+    expectedResult: 'expected_stylish.txt',
+    format: 'stylish',
+    title: 'json json to stylish tree structure',
+  },
+  {
+    filepath1: 'file1.json',
+    filepath2: 'file2.json',
+    expectedResult: 'expected_plain.txt',
+    format: 'plain',
+    title: 'json json to plain text',
+  },
+  {
+    filepath1: 'file1.json',
+    filepath2: 'file2.json',
+    expectedResult: 'expected_json.txt',
+    format: 'json',
+    title: 'json json to json string',
+  },
+  {
+    filepath1: 'file1.yaml',
+    filepath2: 'file2.yml',
+    expectedResult: 'expected_stylish.txt',
+    format: 'stylish',
+    title: 'yaml yml to stylish tree structure',
+  },
+  {
+    filepath1: 'file1.yaml',
+    filepath2: 'file2.yml',
+    expectedResult: 'expected_plain.txt',
+    format: 'plain',
+    title: 'yaml yml to plain text',
+  },
+  {
+    filepath1: 'file1.yaml',
+    filepath2: 'file2.yml',
+    expectedResult: 'expected_json.txt',
+    format: 'json',
+    title: 'yaml yml to json string',
+  },
+];
 
-test('compare stylish', () => {
-  const expectedStylish = readFile('expected_stylish.txt');
-  expect(genDiff(filepath1, filepath2)).toBe(expectedStylish);
-});
-
-test('compare plain', () => {
-  const expectedPlain = readFile('expected_plain.txt');
-  expect(genDiff(filepath3, filepath4, 'plain')).toBe(expectedPlain);
-});
-
-test('compare json', () => {
-  const expectedJson = readFile('expected_json.txt');
-  expect(genDiff(filepath3, filepath4, 'json')).toBe(expectedJson);
+test.each(cases)('Test $title', ({
+  filepath1, filepath2, expectedResult, format,
+}) => {
+  const actual = genDiff(getFixturePath(filepath1), getFixturePath(filepath2), format);
+  const expected = readFile(expectedResult);
+  expect(actual).toEqual(expected);
 });
